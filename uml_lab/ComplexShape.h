859eb4e5718803c31c2b79d1c53301922865dee3
adam94pk <adamzadora94@gmail.com>
2016-06-14 11:40:20 +0200

    add shared_ptr

--- uml_lab/ComplexShape.h
+++ uml_lab/ComplexShape.h
@@ -2,13 +2,14 @@
 #include "classes.h"
 #include "Shape.h"
 #include "OPPERATION.h"
+#include <memory>
 
 class ComplexShape : public Shape
 {
-	Shape * shape1;
-	Shape * shape2;
+	std::shared_ptr<Shape> shape1;
+	std::shared_ptr<Shape> shape2;
 	OPPERATION operation;
 public:
-	ComplexShape(Shape * s1, Shape * s2, const Point &p, OPPERATION op);
+	ComplexShape(std::shared_ptr<Shape> &s1, std::shared_ptr<Shape> &s2, const Point &p, OPPERATION op);
 	bool in(const Point &p) const;
 };
--- uml_lab/uml_lab.cpp
+++ uml_lab/uml_lab.cpp
@@ -8,15 +8,16 @@
 #include "ComplexShape.h"
 #include "Point.h"
 #include <iostream>
+#include <memory>
 
 int main()
 {
 	Point point_c(2., 2.);
-	Circle circle(point_c, 1.);
+	std::shared_ptr<Shape> circle = std::make_shared<Circle>(point_c, 1.);
 	Point point_r(5., 4.);
-	Rectangle rectangle(point_r, 2., 2.);
+	std::shared_ptr<Shape> rectangle = std::make_shared<Rectangle>(point_r, 2., 2.);
 	Point point_complex(3., 3.);
-	ComplexShape complex(&circle, &rectangle, point_complex, sum);
+	ComplexShape complex(circle, rectangle, point_complex, sum);
 	Point p_sum(2., 2.);
 	Point p_sum2(4.5, 4.);
 	Point p_il(4.5, 4.);
